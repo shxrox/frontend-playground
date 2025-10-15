4c6f280eab00d0edb0acb4128054f3c48cc156a2
shxrox <sharondeva8@gmail.com>
2025-10-15 18:27:12 +0530

    Home Page

--- src/componts/Dashbord/Dashbord.jsx
+++ src/componts/Dashbord/Dashbord.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Dashbord() {
   return (
-    <div>Dashbord</div>
+    <div className="bg-[url('../src/assets/homepage.png')] min-h-screen bg-cover bg-center">
+        <div className='flex justify-around'>
+            <div className='pr-100'>
+                <h1 className='text-white text-5xl pt-60 font-extrabold font-serif'>TIMELESS <span className=' text-amber-300'> LUXURY.</span></h1>
+                <p className='text-white text-[20px] pt-5 font-bold font-serif'> ELEGANCE IN EVERY SECOND 
+                    <br />DISCOVER WATCHES THAT DEFINE YOUR STYLE.</p>
+            </div>
+            <div>
+                <h1 className=' '></h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/componts/Navbar/Navbar.jsx
+++ src/componts/Navbar/Navbar.jsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className=' text-white bg-[#000000f8]'>
+    <div className=' text-white bg-[#000000]'>
       <div className='flex justify-between mx-20 py-6 font-serif'>
         <div>
           <Link to="#">
-           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock9-icon lucide-clock-9"><path d="M12 6v6H8"/><circle cx="12" cy="12" r="10"/></svg>
+           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clock9-icon lucide-clock-9"><path d="M12 6v6H8"/><circle cx="12" cy="12" r="10"/></svg>
            </Link>
         </div>
         <div className='flex justify-between space-x-7 '>
           <Link to="/">Home</Link>
-          <Link to="/#">About</Link>
+          <Link to="/Products">Products</Link>
           <Link to="/#">Projects</Link>
           <Link to="/#">Contact us</Link>
         </div>
