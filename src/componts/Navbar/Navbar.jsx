33c7690b4651a61ff45d2a4f812c6d30a4cf2f8b
shxrox <sharondeva8@gmail.com>
2025-10-15 17:33:53 +0530

    Nav icon

--- src/componts/Navbar/Navbar.jsx
+++ src/componts/Navbar/Navbar.jsx
@@ -6,8 +6,8 @@ function Navbar() {
       <div className='flex justify-between mx-20 py-6 font-serif'>
         <div>
           <Link to="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shirt-icon lucide-shirt"><path d="M20.38 3.46 16 2a4 4 0 0 1-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z" /></svg>
-          </Link>
+           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock9-icon lucide-clock-9"><path d="M12 6v6H8"/><circle cx="12" cy="12" r="10"/></svg>
+           </Link>
         </div>
         <div className='flex justify-between space-x-7 '>
           <Link to="/">Home</Link>
